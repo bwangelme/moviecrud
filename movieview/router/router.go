ccb3770e9954d19024f5ea15d2ca5227e5fff0fa
bwangelme <bwangel.me@gmail.com>
2021-01-10 16:43:36 +0800

    add comment

--- movieview/router/router.go
+++ movieview/router/router.go
@@ -21,6 +21,7 @@ func Load(middleware ...gin.HandlerFunc) http.Handler {
 	api := r.Group("/api")
 	api.Use(func(c *gin.Context) {
 		c.Next()
+		//TODO: 如果 error 有多层，那么或生成一个 error 数组，应该处理成只在一个error中返回
 		if len(c.Errors) > 0 {
 			c.JSON(http.StatusBadRequest, c.Errors)
 		}
